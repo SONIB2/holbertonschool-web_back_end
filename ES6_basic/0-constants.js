a46e0976cad983e70a9d68935ad860f891240c78
SONIB2 <soniballa2@gmail.com>
2025-01-11 20:22:40 +0100

    backend

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,21 +1,17 @@
 // 0-constants.js
 
-// Use 'const' since 'task' value should not be reassigned
 export function taskFirst() {
-    const task = 'I prefer const when I can.';  // Use const here
+    const task = 'I prefer const when I can.';  // Use const as the value is not reassigned
     return task;
   }
   
-  // Function to return the string ' is okay'
   export function getLast() {
     return ' is okay';
   }
   
-  // Use 'let' since 'combination' value will be reassigned
   export function taskNext() {
-    let combination = 'But sometimes let';  // Use let here
-    combination += getLast();  // Reassign combination
+    let combination = 'But sometimes let';  // Use let as the value is reassigned/modified
+    combination += getLast();
   
     return combination;
-  }
-  
+  }  
