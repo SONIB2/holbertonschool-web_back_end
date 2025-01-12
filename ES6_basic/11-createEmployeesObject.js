f3d071811876136701af267816106fbcf1a3d457
SONIB2 <soniballa2@gmail.com>
2025-01-12 14:04:35 +0100

    create

---
+++ ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,6 @@
+// 11-createEmployeesObject.js
+export default function createEmployeesObject(departmentName, employees) {
+    const employeesObject = {};
+    employeesObject[departmentName] = employees;
+    return employeesObject;
+  }  
