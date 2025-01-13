2d7712b8751f0ac80bee78828a5fa01c6770e934
SONIB2 <soniballa2@gmail.com>
2025-01-13 17:34:13 +0100

    report

--- ES6_basic/4-main.js
+++ ES6_basic/4-main.js
@@ -1,4 +1,4 @@
 import returnHowManyArguments from './4-rest-parameter.js';
 
-console.log(returnHowManyArguments("one"));                        // Expected output: 1
-console.log(returnHowManyArguments("one", "two", 3, "4th"));       // Expected output: 4
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
+  const budget = {
       income,
       gdp,
       capita,
-    };
-  
-    return budget;
-  }
-  
+};
+return budget;
+}
