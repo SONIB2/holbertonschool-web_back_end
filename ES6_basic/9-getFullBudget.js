950239800b4a2607e88eebb4f81dba48d140fccb
SONIB2 <soniballa2@gmail.com>
2025-01-13 18:46:30 +0100

    report

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,16 +1,15 @@
+/*eslint-disable */
 import getBudgetObject from './7-getBudgetObject.js';
-
 export default function getFullBudgetObject(income, gdp, capita) {
-    const budget = getBudgetObject(income, gdp, capita);
-    const fullBudget = {
-        ...budget,
-        getIncomeInDollars: function (income) {
-            return `$${income}`;
-        },
-        getIncomeInEuros: function (income) {
-            return `${income} euros`;
-        },
-    };
-
-    return fullBudget;
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => {
+      return `$${income}`;
+    },
+    getIncomeInEuros: (income) => {
+      return `${income} euros`;
+    },
+  };
+  return fullBudget;
 }
