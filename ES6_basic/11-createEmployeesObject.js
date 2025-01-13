d73c0b22a82c2e616f9b8e4b014f75fa1936e24e
SONIB2 <soniballa2@gmail.com>
2025-01-13 19:50:00 +0100

    report

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,17 @@
-export default function createEmployeesObject(departmentName, employees) {
-return {
-    [departmentName]: [ ...employees ]
+/* eslint-disable */
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => {
+      return `$${income}`;
+    },
+    getIncomeInEuros: (income) => {
+      return `${income} euros`;
+    },
   };
+
+  return fullBudget;
 }
