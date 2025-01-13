a82ab08f926d6d66cc589beee806ee18421b8aad
SONIB2 <soniballa2@gmail.com>
2025-01-13 18:49:16 +0100

    report

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,8 @@
-// 12-createReportObject.js
+
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: employeesList, // Maps to the employeesList passed as a parameter
-      getNumberOfDepartments: function() {
-        return Object.keys(employeesList).length; // Returns the number of departments (keys in employeesList)
+  return {
+    allEmployees: employeesList, 
+return Object.keys(employeesList).length; 
       }
-    };
-  }
+    }
   
