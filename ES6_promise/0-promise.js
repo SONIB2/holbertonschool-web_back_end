694151943073ef5470edc9c0c1b0ab0139032d2c
SONIB2 <soniballa2@gmail.com>
2025-01-14 22:13:13 +0100

    report

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,6 @@
-// 0-promise.js
-
-function getResponseFromAPI() {
-    // Returning a Promise that resolves with a sample response
-    return new Promise((resolve, reject) => {
-      // Simulating an async operation (e.g., API call)
-      resolve("Response from API");
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+export default function getResponseFromApi() {
+	return new Promise((resolve, reject) => {
+		resolve('');
+		reject(new Error('Something bad happend'));
+	});
+}
