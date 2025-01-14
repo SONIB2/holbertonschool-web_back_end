5bce22367475505aa0f4e83b8cf63ae0155abe90
SONIB2 <soniballa2@gmail.com>
2025-01-14 22:56:27 +0100

    report

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,20 @@
+// 9-try.js
+
+export default function guardrail(mathFunction) {
+    const queue = [];
+    
+    try {
+      // Try to execute the mathFunction and push its result to the queue
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      // If an error is thrown, push the error message to the queue
+      queue.push(error.message);
+    }
+    
+    // Always push this message at the end of the queue
+    queue.push('Guardrail was processed');
+    
+    return queue;
+  }
+  
