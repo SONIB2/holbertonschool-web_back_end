11f3be785704e7f94de14c0f32261042092e757c
SONIB2 <soniballa2@gmail.com>
2025-01-19 20:56:29 +0100

    class

--- ES6_classes/5-building_constructor.test.js
+++ ES6_classes/5-building_constructor.test.js
@@ -1,4 +1,4 @@
-import Building from './5-building.js'; // Correct path to the Building class
+import Building from './building'; // Correct path to the Building class
 
 describe('Building Class', () => {
     test("Building is implemented correctly", () => {
---
+++ ES6_classes/building.js
@@ -0,0 +1,16 @@
+// building.js
+class Building {
+    constructor(sqft) {
+      if (this.constructor === Building) {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+      }
+      this.sqft = sqft;
+    }
+  
+    evacuationWarningMessage() {
+      throw new Error("evacuationWarningMessage must be implemented");
+    }
+  }
+  
+  export default Building;
+  
