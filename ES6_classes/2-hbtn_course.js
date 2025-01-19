03ceaf5c1ae5d6267d98ced29949bec9c6f5fc9b
SONIB2 <soniballa2@gmail.com>
2025-01-19 18:04:15 +0100

    class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -25,21 +25,21 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
-  get name(name) {
+  set name(name) {
     if (typeof name === 'string') {
       this._name = name;
     } else {
       throw new Error('Name must be a string');
     }
   }
-  get length(length) {
+  set length(length) {
     if (typeof length === 'number') {
       this._length = length;
     } else {
       throw new Error('Length must be a number');
     }
   }
-  get students(students) {
+  set students(students) {
     if (Array.isArray(students)) {
       this._students = students;
     } else {
