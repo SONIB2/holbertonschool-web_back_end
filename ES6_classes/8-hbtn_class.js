6f254c87339ad17c153ae2714d2b6e33294cc8d8
SONIB2 <soniballa2@gmail.com>
2025-01-19 20:04:09 +0100

    class

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,19 @@
+class HolbertonClass {
+    constructor(size, location) {
+      this._size = size;
+      this._location = location;
+    }
+  
+    // When the class is cast to a Number, return the size
+    valueOf() {
+      return this._size;
+    }
+  
+    // When the class is cast to a String, return the location
+    toString() {
+      return this._location;
+    }
+  }
+  
+  export default HolbertonClass;
+  
