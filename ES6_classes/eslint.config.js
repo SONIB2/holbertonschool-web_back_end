ce7b89f194b28688c3ea3516d57e8584bd8a5847
SONIB2 <soniballa2@gmail.com>
2025-01-19 18:55:32 +0100

    class

---
+++ ES6_classes/eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+module.exports = {
+    extends: ['eslint:recommended'], // Inherits the recommended ESLint rules
+    parserOptions: {
+      ecmaVersion: 2021, // Enables ECMAScript 2021 features
+      sourceType: 'module', // Allows the use of import/export syntax
+    },
+    env: {
+      browser: true, // Define global variables available in the browser
+      node: true, // Define global variables available in Node.js
+      es2021: true, // Enable ECMAScript 2021 globals
+    },
+    rules: {
+      // Customize rules for your project
+      'no-trailing-spaces': 'error', // Example rule to disallow trailing spaces
+      'no-unused-vars': 'warn', // Warn about unused variables
+      'eqeqeq': 'error', // Enforce strict equality (===)
+    },
+  };
+  
