1ddeddd9f1c8a838330947be5d083ab83ea7681c
SONIB2 <soniballa2@gmail.com>
2025-01-19 20:58:38 +0100

    class

---
+++ ES6_classes/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Other configurations...
+    // Remove this line
+    // extensionsToTreatAsEsm: ['.js'],
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node'], // Keep this line
+  };
+  
