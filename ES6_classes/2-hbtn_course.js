c2cbaaa45d2991508471d7fe0e91d5d77c1761a3
SONIB2 <soniballa2@gmail.com>
2025-01-19 01:55:44 +0100

    class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -35,7 +35,7 @@ class HolbertonCourse {
     return this._students;
   }
   set students(newStudents) {
-    if (!Array.isArray(newStudents) () !newStudents.every(student => typeof student === 'string')) {
+    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
