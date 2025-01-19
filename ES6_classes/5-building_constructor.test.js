f30ca939d768851969bbf82de8ddabed987d78a2
SONIB2 <soniballa2@gmail.com>
2025-01-19 21:08:25 +0100

    class

--- ES6_classes/5-building_constructor.test.js
+++ ES6_classes/5-building_constructor.test.js
@@ -1,12 +1,14 @@
 import Building from './5-building.js'; // Ensure this path is correct
 
 describe('Building Class', () => {
+    // Test to ensure that Building cannot be instantiated directly
     test("Building forces override", () => {
         expect(() => {
             new Building(200); // This should throw an error
         }).toThrowError("Class extending Building must override evacuationWarningMessage");
     });
 
+    // Test for a subclass implementation
     test("TestBuilding is implemented correctly", () => {
         class TestBuilding extends Building {
             evacuationWarningMessage() {
