41cc03633d5fff51e76341ddd5bab8b8ca5229a1
SONIB2 <soniballa2@gmail.com>
2025-01-19 19:15:32 +0100

    class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,4 +3,10 @@ class Building {
     constructor(sqft) {
         this._sqft = sqft; // Store sqft in a private attribute
     }
+    get sqft() {
+        return this._sqft; // Getter for sqft
+    }
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
 }
