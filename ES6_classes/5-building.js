b358bff24ae682413d0321ab3826a8dba50396d1
SONIB2 <soniballa2@gmail.com>
2025-01-19 19:05:18 +0100

    class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+class Building {
+    constructor(sqft) {
+      if (new.target === Building) {
+        throw new Error("Cannot instantiate an abstract class");
+      }
+      this._sqft = sqft;
+    }
+    get sqft() {
+      return this._sqft;
+    }
+    evacuationWarningMessage() {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+  export default Building;
+  
