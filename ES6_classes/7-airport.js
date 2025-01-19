b9ff9e3d8b20da1bf4fd1909b5e0d8e47bc83581
SONIB2 <soniballa2@gmail.com>
2025-01-19 20:00:36 +0100

    class

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 class Airport {
   constructor(name, code) {
     this._name = name;
-  this._code = code;
+    this._code = code;
   }
   toString() {
-  return `[object ${this._code}]`;
-    }
+    return `[object ${this._code}]`;
   }
+}
 export default Airport;
