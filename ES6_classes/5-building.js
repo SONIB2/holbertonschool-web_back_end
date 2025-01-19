211caf65662f0c694bd9dd8609c4f1506cb73f49
SONIB2 <soniballa2@gmail.com>
2025-01-19 19:06:53 +0100

    class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,21 @@
-class Building {
-    constructor(sqft) {
-      if (new.target === Building) {
-        throw new Error("Cannot instantiate an abstract class");
-      }
-      this._sqft = sqft;
-    }
-    get sqft() {
-      return this._sqft;
-    }
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+import Building from './5-building.js';
+class TestBuilding extends Building {
+  evacuationWarningMessage() {
+    return "Evacuation warning!";
   }
-  export default Building;
-  
+}
+describe("Building class", () => {
+  it("throws error when trying to instantiate Building directly", () => {
+    expect(() => new Building(100)).toThrow("Cannot instantiate an abstract class");
+  });
+  it("can be instantiated via subclass", () => {
+    const b = new TestBuilding(100);
+    expect(b.sqft).toBe(100);
+  });
+  it("throws error if subclass does not implement evacuationWarningMessage", () => {
+    class InvalidBuilding extends Building {}
+    expect(() => new InvalidBuilding(100)).toThrow(
+      "Class extending Building must override evacuationWarningMessage"
+    );
+  });
+});
