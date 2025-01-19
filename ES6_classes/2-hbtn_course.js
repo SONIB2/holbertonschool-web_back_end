c9fe40c5064552d6d77bc04fdc69d71d8faeaa72
SONIB2 <soniballa2@gmail.com>
2025-01-19 18:38:08 +0100

    class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -35,10 +35,10 @@ class HolbertonCourse {
     return this._students;
   }
   set students(newStudents) {
-    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
   }
 }
-  export default HolbertonCourse;
+export default HolbertonCourse;
