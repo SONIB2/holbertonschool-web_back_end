fa9c1b4bc1790cd6a3e4dd92c434ca523be111ba
SONIB2 <soniballa2@gmail.com>
2025-01-19 19:10:16 +0100

    class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,28 +1,21 @@
-// 5-building_constructor.test.js
-import Building from './5-building.js';
-
-// Define a subclass that implements the method
-class TestBuilding extends Building {
-  evacuationWarningMessage() {
-    return "Evacuation warning!";
+class Building {
+    constructor(sqft) {
+      if (new.target === Building) {
+        throw new Error("Cannot instantiate an abstract class");
+      }
+      this._sqft = sqft;
+    }
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  
+    // Method to be implemented by subclasses
+    evacuationWarningMessage() {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
-}
-
-describe("Building class", () => {
-  it("throws error when trying to instantiate Building directly", () => {
-    expect(() => new Building(100)).toThrow("Cannot instantiate an abstract class");
-  });
-
-  it("can be instantiated via subclass", () => {
-    const b = new TestBuilding(100);
-    expect(b.sqft).toBe(100);
-  });
-
-  it("throws error if subclass does not implement evacuationWarningMessage", () => {
-    class InvalidBuilding extends Building {}
-
-    expect(() => new InvalidBuilding(100)).toThrow(
-      "Class extending Building must override evacuationWarningMessage"
-    );
-  });
-});
+  
+  export default Building;
+  
--- ES6_classes/5-main.js
+++ ES6_classes/5-main.js
@@ -1,9 +1,13 @@
+// 5-main.js
 import Building from './5-building.js';
-const b = new Building(100);
+
+const b = new Building(100); // This will throw an error as Building is abstract
 console.log(b);
+
 class TestBuilding extends Building {}
+
 try {
-    new TestBuilding(200);
+    new TestBuilding(200); // This will throw an error due to missing evacuationWarningMessage
 } catch (err) {
-    console.log(err);
+    console.log(err); // Output: Error: Class extending Building must override evacuationWarningMessage
 }
