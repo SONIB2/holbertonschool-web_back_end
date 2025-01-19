b4be779dfe98c3b58740ae9a6c8d11c92c27dd5d
SONIB2 <soniballa2@gmail.com>
2025-01-19 20:21:55 +0100

    class

--- ES6_classes/9-hoisting.length.test.js
+++ ES6_classes/9-hoisting.length.test.js
@@ -1,6 +1,6 @@
 import { listOfStudents } from './9-hoisting.js';
 
-console.log('Imported listOfStudents:', listOfStudents); // Debug log
+console.log(listOfStudents); // Check what is being imported
 
 test("listOfStudents has the correct length", () => {
   expect(listOfStudents.length).toBe(5);
