dd3ca155498bd8295e9caf675dcf06b040ea8fbc
SONIB2 <soniballa2@gmail.com>
2025-01-19 02:00:52 +0100

    class

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,46 @@
+class Currency {
+    constructor(code, name) {
+      if (typeof code !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._code = code;
+      this._name = name;
+    }
+  
+    // Getter for code
+    get code() {
+      return this._code;
+    }
+  
+    // Setter for code
+    set code(newCode) {
+      if (typeof newCode !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      this._code = newCode;
+    }
+  
+    // Getter for name
+    get name() {
+      return this._name;
+    }
+  
+    // Setter for name
+    set name(newName) {
+      if (typeof newName !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = newName;
+    }
+  
+    // Method to display the full currency
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
+  }
+  
+  export default Currency;
+  
