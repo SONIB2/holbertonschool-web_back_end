24cc5ce03d7649f72618497e8ffdd88999ff34d3
SONIB2 <soniballa2@gmail.com>
2025-01-19 19:57:48 +0100

    class

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    // Overriding the toString method to return the airport code
+    toString() {
+      return `[object ${this._code}]`;
+    }
+  }
+  
+  export default Airport;
+  
