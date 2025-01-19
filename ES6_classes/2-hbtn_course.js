3bf55f8dba7d7c6e6bc4acc1a3fe28a93a284d17
SONIB2 <soniballa2@gmail.com>
2025-01-19 18:44:12 +0100

    class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -16,6 +16,7 @@ class HolbertonCourse {
   get name() {
     return this._name;
   }
+  
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
