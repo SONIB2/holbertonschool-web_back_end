dd73d6066761fb914d4f8aafd51c91a0adba901e
SONIB2 <soniballa2@gmail.com>
2025-01-19 19:14:10 +0100

    class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,31 +1,6 @@
-// 5-building_constructor.test.js
-import Building from './5-building.js';
-
-// Define a subclass that implements the evacuationWarningMessage method
-class TestBuilding extends Building {
-  evacuationWarningMessage() {
-    return "Evacuation warning!";
-  }
+// 5-building.js
+class Building {
+    constructor(sqft) {
+        this._sqft = sqft; // Store sqft in a private attribute
+    }
 }
-
-describe("Building class", () => {
-  // Test to ensure that we cannot instantiate the abstract class directly
-  it("throws error when trying to instantiate Building directly", () => {
-    expect(() => new Building(100)).toThrow("Cannot instantiate an abstract class");
-  });
-
-  // Test that subclass can be instantiated
-  it("can be instantiated via subclass", () => {
-    const b = new TestBuilding(100);
-    expect(b.sqft).toBe(100);  // Ensure sqft is set correctly
-  });
-
-  // Test to ensure that error is thrown if subclass doesn't implement evacuationWarningMessage
-  it("throws error if subclass does not implement evacuationWarningMessage", () => {
-    class InvalidBuilding extends Building {}
-
-    expect(() => new InvalidBuilding(100)).toThrow(
-      "Class extending Building must override evacuationWarningMessage"
-    );
-  });
-});
--- ES6_classes/5-main.js
+++ ES6_classes/5-main.js
@@ -1,13 +1,12 @@
-// 5-main.js
 import Building from './5-building.js';
 
-const b = new Building(100); // This will throw an error as Building is abstract
-console.log(b);
+const b = new Building(100);
+console.log(b); // Output: Building { _sqft: 100 }
 
 class TestBuilding extends Building {}
 
 try {
-    new TestBuilding(200); // This will throw an error due to missing evacuationWarningMessage
+    new TestBuilding(200);
 } catch (err) {
     console.log(err); // Output: Error: Class extending Building must override evacuationWarningMessage
 }
