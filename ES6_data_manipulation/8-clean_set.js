1c9c862a48898a557eefc210589bc6e99a09361f
SONIB2 <soniballa2@gmail.com>
2025-01-20 22:39:55 +0100

    data

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
 export default function cleanSet(set, startString) {
+    // If the startString is empty, return all items from the set
+    if (startString === '') {
+      return [...set].join('-');
+    }
+  
     // Create an array of strings that start with 'startString'
     const result = [];
-    
+  
     // Loop through each element in the set
     for (let item of set) {
       // If the item starts with the startString
@@ -10,7 +15,7 @@ export default function cleanSet(set, startString) {
         result.push(item.slice(startString.length));
       }
     }
-    
+  
     // Join the array into a string separated by '-'
     return result.join('-');
   }
