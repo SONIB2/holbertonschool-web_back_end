d7c4eed8dac7d22e5a1074ee4525acbe8d92124b
SONIB2 <soniballa2@gmail.com>
2025-01-20 21:45:27 +0100

    data

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 function getStudentIdsSum(students) {
   // Use reduce to sum the ids of all students
   return students.reduce((sum, student) => sum + student.id, 0);
-} 
+}
 export default getStudentIdsSum;
