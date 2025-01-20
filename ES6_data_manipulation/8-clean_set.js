955c79a661ee9819c59d6238128716c65bf0e2d7
SONIB2 <soniballa2@gmail.com>
2025-01-20 22:27:44 +0100

    data

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+function cleanSet(set, startString) {
+  return [...set]
+    .filter(item => item.startsWith(startString))  // Filter values that start with startString
+    .map(item => item.slice(startString.length))   // Remove startString from each element
+    .join('-');                                   // Join the values with '-'
+}
+export default cleanSet;
