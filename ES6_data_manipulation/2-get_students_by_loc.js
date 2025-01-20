083e0ea4933a4ee82b954f569a5b260989d5e0c8
SONIB2 <soniballa2@gmail.com>
2025-01-20 21:43:23 +0100

    data

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 function getStudentsByLocation(students, city) {
   // Use filter to return students located in the specified city
   return students.filter((student) => student.location === city);
-}  
+}
 export default getStudentsByLocation;
