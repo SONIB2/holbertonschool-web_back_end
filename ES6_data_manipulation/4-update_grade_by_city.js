77954197f24c36b28e7d1a3d43d0908166bccc14
SONIB2 <soniballa2@gmail.com>
2025-01-20 21:50:19 +0100

    data

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
   // Filter students by the specified city
-  .filter(student => student.location === city)
+    .filter(student => (student).location === city)
   // Map the filtered students to include their updated grades
-  .map(student => {
-  // Find the grade for the student in newGrades, if any
-    const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+    .map((student) => {
+      // Find the grade for the student in newGrades, if any
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
   // If a grade is found, use it; otherwise, set grade to 'N/A'
-    student.grade = gradeObj ? gradeObj.grade : 'N/A';
-  return student;
+      student.grade = gradeObj ? gradeObj.grade : 'N/A';
+      return student;
     });
 }
 export default updateStudentGradeByCity;
