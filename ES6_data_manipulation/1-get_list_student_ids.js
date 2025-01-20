19c82b3fbd312eccc10854cca1a03249755e0cc2
SONIB2 <soniballa2@gmail.com>
2025-01-20 21:37:11 +0100

    data

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,4 +5,4 @@ function getListStudentIds(students) {
   // Add parentheses around the arrow function argument
   return students.map((student) => student.id);
 }
-  export default getListStudentIds;
+export default getListStudentIds;
