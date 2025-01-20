34e4ead376e536d3c0b83ea7baebd4a6a0c5d083
SONIB2 <soniballa2@gmail.com>
2025-01-20 22:31:10 +0100

    data

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,19 @@
 export default function cleanSet(set, startString) {
-	const arr = [];
-	if (typeof startString === 'undefined' || startString === '' || typeof startString !== 'string') {
-		return '';
-	}
-	set.forEach((element) => {
-		if (typeof element !== 'undefined') {
-			if (element.startsWith(startString)) {
-				arr.push(element.split(startString)[1]);
-			}
-		}
-	});
-	return arr.join('-');
-}
+    const arr = [];
+  
+    // Check if startString is valid
+    if (typeof startString === 'undefined' || startString === '' || typeof startString !== 'string') {
+      return '';
+    }
+  
+    set.forEach((element) => {
+      // If element starts with startString, remove startString and push the rest
+      if (element.startsWith(startString)) {
+        arr.push(element.slice(startString.length));
+      }
+    });
+  
+    // If arr is empty, return an empty string, else join the array with hyphen
+    return arr.length > 0 ? arr.join('-') + '-' : '';
+  }
+  
