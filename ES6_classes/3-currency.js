de23394e3d0e5b6761285b3ab3c244088604a2f3
Soni Balla <145006206+SONIB2@users.noreply.github.com>
2025-01-20 21:03:11 +0100

    Update 3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -9,26 +9,32 @@ class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(newCode) {
     if (typeof newCode !== 'string') {
       throw new TypeError('Code must be a string');
     }
     this._code = newCode;
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
+
 export default Currency;
