2ca7423c784e75fc5a68a73d765b695eaf11f502
Soni Balla <145006206+SONIB2@users.noreply.github.com>
2025-01-20 20:55:50 +0100

    Update 8-hbtn_class.js

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -3,11 +3,14 @@ class HolbertonClass {
     this._size = size;
     this._location = location;
   }
+  
   valueOf() {
     return this._size;
   }
+  
   toString() {
     return this._location;
   }
 }
+
 export default HolbertonClass;
