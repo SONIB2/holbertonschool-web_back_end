3662bb960f164acb0d7186ea4dcb26a46eac8b52
SONIB2 <soniballa2@gmail.com>
2025-02-18 21:00:26 +0100

    sql

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,14 +1,4 @@
--- Create the table if it does not already exist
-CREATE TABLE IF NOT EXISTS metal_bands (
-    origin VARCHAR(255) NOT NULL,
-    nb_fans INT NOT NULL
-);
-
--- Insert data into the table (you can also import the dump here if it’s a separate file)
--- Example: Insert some data (adjust this based on actual data or import method)
--- INSERT INTO metal_bands (origin, nb_fans) VALUES ('USA', 99349), ('Sweden', 47169), ... ;
-
--- Rank countries by the number of fans, ordered by nb_fans in descending order
-SELECT origin, nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
