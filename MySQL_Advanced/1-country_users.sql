073cdd62a832536255e7b05bd3045dcfa7a6bb5d
SONIB2 <soniballa2@gmail.com>
2025-02-18 18:58:04 +0100

    country user enum

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 --create table users--
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    PRIMARY KEY(id)
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
