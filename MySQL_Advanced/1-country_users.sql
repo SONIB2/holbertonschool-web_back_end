b2c58c4117963d60e5474d7ea629324e61f00fe2
SONIB2 <soniballa2@gmail.com>
2025-02-18 20:53:19 +0100

    sql

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,13 @@
---create table users--
+-- Disable foreign key checks temporarily
+SET foreign_key_checks = 0;
+
+-- Create the users table
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255),
-    PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
+
+-- Enable foreign key checks back
+SET foreign_key_checks = 1;
