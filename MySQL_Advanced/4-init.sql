d14d714bf26aee1624b0d5919700c17c2b636c03
SONIB2 <soniballa2@gmail.com>
2025-02-18 19:00:30 +0100

    country user enum

---
+++ MySQL_Advanced/4-init.sql
@@ -0,0 +1,7 @@
+-- Insert sample orders
+INSERT INTO orders (item_name, number) VALUES ('apple', 1);
+INSERT INTO orders (item_name, number) VALUES ('apple', 3);
+INSERT INTO orders (item_name, number) VALUES ('pear', 2);
+
+-- Check the updated items table
+SELECT * FROM items;
