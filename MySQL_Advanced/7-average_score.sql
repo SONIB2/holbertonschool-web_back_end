dd38e04b51990232e73043c2bc4d246001f2d7c9
SONIB2 <soniballa2@gmail.com>
2025-02-20 18:38:33 +0100

    sql

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,10 @@
+-- Drop the existing procedure if it exists
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+-- Create the procedure to compute and store the average score for a user
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
@@ -9,7 +13,7 @@ BEGIN
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update the user's average score
+    -- Update the user's average score in the users table
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
